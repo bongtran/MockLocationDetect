c91f692945e8e877a17b25bf1062f71117d81211
DESKTOP-E7Q49G8\Shazam_ORG <letter.com.vn@gmail.com>
2017-11-16 17:28:25 +0700

    Add more checking func

--- app/src/main/java/com/bongtran/mocklocationchecker/MainActivity.java
+++ app/src/main/java/com/bongtran/mocklocationchecker/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -13,21 +15,39 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity implements LocationListener, View.OnClickListener,  GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
+    private static final int LOCATION_MODE_HIGH_ACCURACY = 1;
     private TextView txtVersion, txtMocked, txtMockedApp, txtMockedLocation;
-
+    private boolean mockLocationsEnabled;
     private String TAG = "MOCKING CHECK";
     LocationManager lm;
+    int numGoodReadings = 0;
+    Location lastMockLocation;
     @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +58,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         txtVersion = findViewById(R.id.txtVersion);
         txtMockedApp = findViewById(R.id.txtMockedApp);
         txtMockedLocation = findViewById(R.id.txtMockedLocation);
+
+        findViewById(R.id.btnSetting).setOnClickListener(this);
 
         txtVersion.setText("API Version " + Build.VERSION.CODENAME + " " + Build.VERSION.SDK_INT);
         lm = (LocationManager) getSystemService(LOCATION_SERVICE);
@@ -52,7 +74,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         try {
             Log.d(TAG, "Removing Test providers");
             lm.removeTestProvider(LocationManager.GPS_PROVIDER);
-        } catch (IllegalArgumentException error) {
+        } catch (Exception error) {
             Log.d(TAG, "Got exception in removing test  provider");
         }
         boolean isMock = isMockSettingsON(this);
@@ -128,12 +150,10 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(Location location) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-            String locationString = location.getLatitude() + " " + location.getLongitude();
-            if(location.isFromMockProvider())
-                locationString += "FROM MOCKED LOCATION";
+        boolean plausible = isLocationPlausible(location);
 
-            txtMockedLocation.setText(locationString);
+        if (!plausible) {
+            txtMockedLocation.setText("MOCKED LOCATION");
         }
     }
 
@@ -224,5 +244,132 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             Toast.makeText(this, "DENIED", Toast.LENGTH_LONG).show();
             finish();
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id){
+            case R.id.btnSetting:
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                break;
+                default:
+                    break;
+        }
+    }
+
+    private void checkMockLocations() {
+        // Starting with API level >= 18 we can (partially) rely on .isFromMockProvider()
+        // (http://developer.android.com/reference/android/location/Location.html#isFromMockProvider%28%29)
+        // For API level < 18 we have to check the Settings.Secure flag
+        if (Build.VERSION.SDK_INT < 18 &&
+                !android.provider.Settings.Secure.getString(this.getContentResolver(), android.provider.Settings
+                        .Secure.ALLOW_MOCK_LOCATION).equals("0")) {
+            mockLocationsEnabled = true;
+
+            txtMockedLocation.setText("MOCKED LOCATION");
+        } else
+            mockLocationsEnabled = false;
+    }
+
+    private boolean isLocationPlausible(Location location) {
+        if (location == null) return false;
+
+        boolean isMock = mockLocationsEnabled || (Build.VERSION.SDK_INT >= 18 && location.isFromMockProvider());
+        if (isMock) {
+            lastMockLocation = location;
+            numGoodReadings = 0;
+        } else
+            numGoodReadings = Math.min(numGoodReadings + 1, 1000000); // Prevent overflow
+
+        // We only clear that incident record after a significant show of good behavior
+        if (numGoodReadings >= 20) lastMockLocation = null;
+
+        // If there's nothing to compare against, we have to trust it
+        if (lastMockLocation == null) return true;
+
+        // And finally, if it's more than 1km away from the last known mock, we'll trust it
+        double d = location.distanceTo(lastMockLocation);
+        return (d > 1000);
+    }
+
+    private void showNetworkSetting(){
+//        GoogleApiClient mGoogleApiClient;
+//        LocationRequest mLocationRequest;
+//
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .addApi(LocationServices.API)
+//                .build();
+//        mGoogleApiClient.connect();
+//
+//        mLocationRequest = new LocationRequest();
+//        mLocationRequest.setInterval(1000);
+//        mLocationRequest.setFastestInterval(1000);
+//        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+//        if (ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+//        }
+//
+//        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+//        PendingResult<LocationSettingsResult> result =
+//                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
+//
+//        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+//            @Override
+//            public void onResult(LocationSettingsResult result) {
+//                final Status status = result.getStatus();
+//                final LocationSettingsStates = result.getLocationSettingsStates();
+//                switch (status.getStatusCode()) {
+//                    case LocationSettingsStatusCodes.SUCCESS:
+//
+//                        break;
+//                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+//                        // Location settings are not satisfied. But could be fixed by showing the user
+//                        // a dialog.
+//                        try {
+//                            // Show the dialog by calling startResolutionForResult(),
+//                            // and check the result in onActivityResult().
+//                            status.startResolutionForResult(
+//                                    MainActivity.this,
+//                                    LOCATION_MODE_HIGH_ACCURACY);
+//                        } catch (IntentSender.SendIntentException e) {
+//                            // Ignore the error.
+//                        }
+//                        break;
+//                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+//                        // Location settings are not satisfied. However, we have no way to fix the
+//                        // settings so we won't show the dialog.
+//
+//                        break;
+//                }
+//            }
+//        });
+//
+//        int locationMode = Settings.Secure.getInt(MainActivity.getContentResolver(), Settings.Secure.LOCATION_MODE);
+//
+//        if(locationMode == LOCATION_MODE_HIGH_ACCURACY) {
+//            //request location updates
+//        } else { //redirect user to settings page
+//            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+//        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
---
+++ app/src/main/java/com/bongtran/mocklocationchecker/REQUEST_CHECK_SETTINGS.java
@@ -0,0 +1,8 @@
+package com.bongtran.mocklocationchecker;
+
+/**
+ * Created by Shazam_ORG on 11/14/2017.
+ */
+
+class REQUEST_CHECK_SETTINGS {
+}
